9a1ef96401cdd7d21cf1f270bfed3f77e1d57704
tranquyetthang281 <79098631+tranquyetthang281@users.noreply.github.com>
2024-01-08 22:52:23 +0700

    Create Database

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string Gym { get; set; }
 
